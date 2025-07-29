9ad3f17e04490fca90caf7bfd956adcedb1a8ac4
Shelby Norris <shelbs9109@gmail.com>
2025-07-29 18:54:05 -0500

    first commit

---
+++ src/greetings.js
@@ -0,0 +1,10 @@
+// ES Modules to greet and say goodbye to users
+// Export to src/index.js
+
+export function sayHello(name) {
+  return `Hello, ${name}!`;
+}
+
+export function sayGoodbye(name) {
+  return `Goodbye, ${name}!`;
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Import ES Modules from src/greetings.js
+
+import { sayHello, sayGoodbye } from "./greetings.js";
+
+console.log(sayHello("Webpack"));
+console.log(sayGoodbye("Webpack"));
